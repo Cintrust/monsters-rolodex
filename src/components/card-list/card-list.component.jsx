7fbb8b90ba1e612a2eef9eeabfad9232e28944ec
Trust <cintrust301@gmail.com>
2021-06-07 14:31:50 +0100

    Updates

--- src/components/card-list/card-list.component.jsx
+++
@@ -1,9 +0,0 @@
-import "./card-list.styles.css"
-import {Card} from "../card/card.component";
-
-
-export const CardList = ({monsters}) => {
-    return (<div className='card-list' >
-        {monsters.map(monster=>(<Card key={monster.id} monster={monster}/>))}
-    </div>);
-}
--- src/components/card/card.component.jsx
+++
@@ -1,9 +0,0 @@
-import "./card.styles.css"
-
-export const Card = ({monster}) => {
-    return (<div className='card-container'>
-        <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt="monster"/>
-        <h2 key={monster.id}>{monster.name}</h2>
-        <p>{monster.email}</p>
-    </div>)
-}
